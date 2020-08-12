3fd28762ea23b65f2a20083ea7c0644b1e49b6dd
Dariusz <darek200180@gmail.com>
2020-08-12 23:43:31 +0100

    add rendering depend of movie state

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -20,32 +20,40 @@ class Movies extends Component {
         })
     }
     
-    render() { 
+    render() {
+        const {length: count} = this.state.movies;
+
+        if(count === 0)
+            return <p>Ther are no movies in the database</p>
+        
         return (  
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Genre</th>
-                        <th scope="col">Stock</th>
-                        <th scope="col">Rate</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.movies.map(movie => (
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <th><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></th>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+            <React.Fragment>
+                <p>Showing {count} movies in the database.</p>            
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Rate</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.movies.map(movie => (
+                                <tr key={movie._id}>
+                                    <td>{movie.title}</td>
+                                    <td>{movie.genre.name}</td>
+                                    <td>{movie.numberInStock}</td>
+                                    <td>{movie.dailyRentalRate}</td>
+                                    <th><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></th>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </React.Fragment>
         );
     }
 }
