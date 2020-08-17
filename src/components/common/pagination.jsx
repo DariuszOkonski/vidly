6c159267fb4c9c92c0dabf7a42e7d17f5840fe2f
Dariusz <darek200180@gmail.com>
2020-08-17 23:33:59 +0100

    build pagination component

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import _ from 'lodash'; //underscore library
 
 const Pagination = (props) => {
+    const {itemsCount, pageSize} = props;
+    const pagesCount = Math.ceil(itemsCount / pageSize);
+    
+    if(pagesCount === 1)
+        return null;
+
+    const pages = _.range(1, pagesCount + 1);
+
     return (  
-        null
+        <nav>
+            <ul className="pagination">
+                {
+                    pages.map(page => (
+                        <li className="page-item" key={page}>
+                            <a className="page-link">{page}</a>
+                        </li>
+                    ))
+                }
+                
+            </ul>
+        </nav>
     );
 }
  
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,7 +6,7 @@ import Pagination from './common/pagination'
 class Movies extends Component {
     state = {  
         movies: [],
-        pageSize: 4
+        pageSize: 10
     }
     
     componentDidMount() {
