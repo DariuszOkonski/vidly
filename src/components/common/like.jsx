d7ce13cb135f5ec30f89dd286eea10634ad8b1a9
Dariusz <darek200180@gmail.com>
2020-08-17 23:08:24 +0100

    change Like component to stateless component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Like extends Component {
-    render() { 
-        let classes = "fa fa-heart";
-        classes += (!this.props.liked) ? "-o" : "";
+const Like = () => {
+    let classes = "fa fa-heart";
+    classes += (!this.props.liked) ? "-o" : "";
 
-        return (
-            <i 
-                onClick={this.props.onClick} 
-                className={classes} 
-                style={{cursor: 'pointer'}}
-                aria-hidden="true"
-            ></i>
-        );
-    }
+    return (
+        <i 
+            onClick={this.props.onClick} 
+            className={classes} 
+            style={{cursor: 'pointer'}}
+            aria-hidden="true"
+        ></i>
+    );
 }
  
 export default Like;
