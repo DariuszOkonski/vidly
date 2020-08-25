0f33835e8f3c53afdd6c20f707860c2e5130187f
Dariusz Okonski <darek200180@gmail.com>
2020-08-25 13:56:30 +0100

    add funcionality for display active page at pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 
 const Pagination = (props) => {
-    const { itemsCount, pageSize} = props;
-
+    const { itemsCount, pageSize, currentPage, onPageChange} = props;
     const pagesCount = Math.ceil(itemsCount / pageSize);
 
     if(pagesCount === 1)
@@ -16,8 +15,11 @@ const Pagination = (props) => {
             <ul className="pagination">
                 {
                     pages.map(page => (
-                        <li key={page} className="page-item">
-                            <a className="page-link">{page}</a>
+                        <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
+                            <a 
+                                className="page-link"
+                                onClick={() => onPageChange(page)}
+                            >{page}</a>
                         </li>
                     ))
                 }
