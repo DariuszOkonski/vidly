ca3879f469b749fb3f6fa21193a980d2b4096bd9
Dariusz Okonski <darek200180@gmail.com>
2020-08-25 13:41:23 +0100

    add pagination component with logic display number of pages

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
+import Pagination from './common/pagination';
 
 class Movies extends Component {
     state = { 
-        movies: getMovies()
+        movies: getMovies(),
+        pageSize: 4,
      }
 
     handleDelete = (movie) => {
@@ -24,6 +26,10 @@ class Movies extends Component {
         this.setState({
             movies
         })
+    }
+
+    handlePageChange = (page) => {
+        console.liked('handlePageChange: ', page);
     }
 
     render() { 
@@ -71,6 +77,12 @@ class Movies extends Component {
                         }
                     </tbody>
                 </table>
+
+                <Pagination 
+                    itemsCount={count}
+                    pageSize={this.state.pageSize}
+                    onPageChange={this.handlePageChange}
+                />
             </React.Fragment>
          );
     }
