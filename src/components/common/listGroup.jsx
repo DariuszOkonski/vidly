f48345ee21e7dd368855f9cb338ecdef574b7492
Dariusz Okonski <darek200180@gmail.com>
2020-08-26 12:30:21 +0100

    create ListGroup component

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ListGroup = () => {
+const ListGroup = (props) => {
+    const { items, onItemSelect, textProperty, valueProperty } = props;
+
     return ( 
-        <div>ListGroup</div>
+        <ul className="list-group">
+            <li className="list-group-item active">Hello</li>
+            {
+                items.map(item => 
+                    <li 
+                        key={item[valueProperty]}
+                        className="list-group-item"
+                    >{item[textProperty]}</li>                    
+                )
+            }
+        </ul>
      );
 }
- 
+
+ListGroup.propTypes = { 
+    items: PropTypes.array.isRequired, 
+    onItemSelect: PropTypes.func.isRequired,
+    textProperty: PropTypes.string.isRequired,
+    valueProperty: PropTypes.string.isRequired
+}
+
 export default ListGroup;
