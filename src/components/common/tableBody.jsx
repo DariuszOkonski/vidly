5bdf815dbf521fee1de74efe87527fd24e4df537
Dariusz Okonski <darek200180@gmail.com>
2020-08-27 12:51:00 +0100

    rendering sell content

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 class TableBody extends Component {
+
+    renderCall = (item, column) => {
+        if(column.content)
+            return column.content(item);
+
+        return _.get(item, column.path)
+    };
+
     render() { 
         const { data, columns } = this.props;
+        console.log(data);
 
         return ( 
             <tbody>
                 {data.map(item => (
                     <tr>
-                        {columns.map(column => <td></td>)}
+                        {columns.map(column => <td>{this.renderCall(item, column)}</td>)}
                     </tr>
                 ))}
 
