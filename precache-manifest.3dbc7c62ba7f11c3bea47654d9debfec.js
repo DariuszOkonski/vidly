f4e792a3f8b46359163b6d2ad1e335b98828a8ba
Dariusz Okonski <darek200180@gmail.com>
2020-08-31 16:20:21 +0100

    Updates

---
+++ precache-manifest.3dbc7c62ba7f11c3bea47654d9debfec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5254a7aada9ca80402767ab4a5b1dce",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "73a18b8ce3ef3575b70e",
+    "url": "/vidly/static/css/2.1cc6a4e2.chunk.css"
+  },
+  {
+    "revision": "f38fd547739566b11001",
+    "url": "/vidly/static/css/main.627a83f7.chunk.css"
+  },
+  {
+    "revision": "73a18b8ce3ef3575b70e",
+    "url": "/vidly/static/js/2.5f4fc1b6.chunk.js"
+  },
+  {
+    "revision": "aa4039141ac8bca354206cb14ee23eb2",
+    "url": "/vidly/static/js/2.5f4fc1b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38fd547739566b11001",
+    "url": "/vidly/static/js/main.da904ee2.chunk.js"
+  },
+  {
+    "revision": "cb388922157c702ba0d5",
+    "url": "/vidly/static/js/runtime-main.f8720289.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
