a8797ff00392fe885f1acb6061efb4db454eb59a
Dariusz Okonski <darek200180@gmail.com>
2020-08-31 15:33:25 +0100

    build bootstrap form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import LoginForm from './components/common/loginForm';
+import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import NotFound from './components/notFound';
 import Rentals from './components/rentals';
 import Movies from './components/movies';
 import NavBar from './components/navBar';
 import './App.css';
-import MovieForm from './components/movieForm';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <main className="container">
         
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
