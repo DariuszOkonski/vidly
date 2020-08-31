4fb3353db89c1e2a8f794ea853bb2efd2dd7d7be
Dariusz Okonski <darek200180@gmail.com>
2020-08-31 14:41:59 +0100

    add routing at component App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
 import Movies from './components/movies';
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect exact from="/" to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
     </main>
   );
 }
